74e12d093bdfb389e94b040b1597cc4ca7297e17
jaydonhashimoto <jaydonhashi@gmail.com>
2019-10-02 22:05:52 -0700

    used loop to render waveforms

--- src/App.js
+++ src/App.js
@@ -3,70 +3,60 @@ import './App.css';
 import WaveSurfer from 'wavesurfer.js';
 
 function App() {
-  let wavesurfer = '';
-  let wavesurfer2 = '';
-  const [isClicked, setIsClicked] = useState(false);
+  const [wave, setWave] = useState([]);
 
   useEffect(() => {
-    wavesurfer = WaveSurfer.create({
-      container: '#waveform'
-    });
-    wavesurfer.setVolume(0.25);
-    wavesurfer.load(
-      'https://jaydon-hashimoto-test-bucket.s3-us-west-1.amazonaws.com/Cm_Blues_BT.mp3'
-    );
-    wavesurfer.on('ready', () => {
-      wavesurfer.play();
-    });
-
-    if (isClicked) {
-      wavesurfer.playPause();
-      setIsClicked(false);
+    for (let i = 0; i < 2; i++) {
+      wave[i] = WaveSurfer.create({
+        container: '#waveform'
+      });
+      wave[i].setVolume(0.25);
     }
 
-    wavesurfer2 = WaveSurfer.create({
-      container: '#waveform2'
-    });
-    wavesurfer2.setVolume(0.25);
-    wavesurfer2.load(
-      'https://jaydon-hashimoto-test-bucket.s3-us-west-1.amazonaws.com/Cm_Blues_BT.mp3'
-    );
-    wavesurfer2.on('ready', () => {
-      wavesurfer2.play();
-    });
+    for (let i = 0; i < 2; i++) {
+      wave[i].load(
+        'https://jaydon-hashimoto-test-bucket.s3-us-west-1.amazonaws.com/Cm_Blues_BT.mp3'
+      );
+      // wave[i].on('ready', () => {
+      //   wave[i].play();
+      // });
+    }
   });
-  const play = () => {
-    // setIsClicked(true);
-    wavesurfer.play();
-  };
-
-  const pause1 = () => {
-    wavesurfer.pause();
-  };
-
-  const play2 = () => {
-    wavesurfer2.play();
-  };
-
-  const pause2 = () => {
-    wavesurfer2.pause();
-  };
 
   return (
     <div className="App" style={container}>
-      <h1>hello</h1>
       <div id="waveform"></div>
-      <button id="playBtn" onClick={play}>
+      <button
+        id="playBtn"
+        onClick={() => {
+          wave[0].play();
+        }}
+      >
         Play
       </button>
-      <button id="pauseBtn" onClick={pause1}>
+      <button
+        id="pauseBtn"
+        onClick={() => {
+          wave[0].pause();
+        }}
+      >
         Pause
       </button>
       <div id="waveform2"></div>
-      <button id="playBtn2" onClick={play2}>
+      <button
+        id="playBtn2"
+        onClick={() => {
+          wave[1].play();
+        }}
+      >
         Play
       </button>
-      <button id="pauseBtn2" onClick={pause2}>
+      <button
+        id="pauseBtn2"
+        onClick={() => {
+          wave[1].pause();
+        }}
+      >
         Pause
       </button>
     </div>
